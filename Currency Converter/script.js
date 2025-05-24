71cadb3489f85a6c61ec5a2289b21db8b0988fc0
Aasthap3 <porwalaastha@gmail.com>
2025-05-24 23:14:50 +0530

    Curr Converter Done

---
+++ Currency Converter/script.js
@@ -0,0 +1,60 @@
+const from = document.getElementById("from");
+const to = document.getElementById("to");
+const fromFlag = document.querySelector("#fromFlag");
+const toFlag = document.querySelector("#toFlag");
+
+
+async function convert() {
+  const amt = document.getElementById("amt").value.trim();
+  const fromCurr = from.value.split(",")[1].toLowerCase();
+  const toCurr = to.value.split(",")[1].toLowerCase();
+
+  const URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurr}.json`;
+
+  const response = await fetch(URL);
+  const data = await response.json();
+
+  const rate = data[fromCurr];
+  const newAmt = amt * rate[toCurr];
+  document.getElementById("converted").innerText = newAmt;
+
+}
+
+async function autofill() {
+  const response = await fetch("./codes.json");
+  const data = await response.json();
+
+  data.forEach((element) => {
+    const newOption = document.createElement("option");
+    newOption.innerText = element.Currency_Code;
+    newOption.value = element.Country_Code + "," + element.Currency_Code;
+
+    from.appendChild(newOption);
+  });
+
+  data.forEach((element) => {
+    const newOption = document.createElement("option");
+    newOption.innerText = element.Currency_Code;
+    newOption.value = element.Country_Code + "," + element.Currency_Code;
+
+    to.appendChild(newOption);
+  });
+
+  from.value = "US,USD";
+  to.value = "IN,INR"
+}
+
+autofill();
+
+from.addEventListener("change", updateFromFlag);
+to.addEventListener("change", updateToFlag);
+
+function updateFromFlag(){
+    let countryCode = from.value.split(",")[0];
+    fromFlag.src = `https://flagsapi.com/${countryCode}/flat/48.png`;
+};
+
+function updateToFlag(){
+    let countryCode = to.value.split(",")[0];
+    toFlag.src = `https://flagsapi.com/${countryCode}/flat/48.png`;
+};
