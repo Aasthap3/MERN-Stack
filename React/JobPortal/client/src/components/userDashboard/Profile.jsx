ff8b2c44c4d5d869b2794762cc71f8de11f74a27
Aasthap3 <porwalaastha@gmail.com>
2025-06-24 17:13:21 +0530

    Data Fetched on User Dashboard

--- React/JobPortal/client/src/components/userDashboard/Profile.jsx
+++ React/JobPortal/client/src/components/userDashboard/Profile.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import photo from "../../assets/profile.jpg";
 
 const Profile = () => {
+  const [data, setData] = useState(JSON.parse(sessionStorage.getItem("user")));
+
   return (
     <>
       <div className="m-5">
         <h1 className="font-mulish text-lg">My Profile</h1>
         <div className="bg-white rounded-lg my-5">
           <div className="details p-5 flex">
-            <div className="bg-gray-200 w-20 h-20 flex items-center justify-center rounded-full text-3xl">
-              A
+            <div>
+              <img src={data.photo} className="w-20 h-20 rounded-full object-cover" alt="" />
             </div>
             <div className="flex flex-col h-20 px-5 justify-center">
-              <span className="font-medium text-lg">Aastha Porwal</span>
+              <span className="font-medium text-lg">{data.firstName} {data.lastName}</span>
               <span className="text-gray-500">Frontend Developer</span>
             </div>
           </div>
@@ -20,15 +22,13 @@ const Profile = () => {
             <div className="grid w-1/2">
               <span className="font-bold">Contact Information</span>
               <span className="text-md text-gray-500">
-                Email: porwalaastha@gmail.com
+                Email: {data.email}
               </span>
-              <span className="text-md text-gray-500">Phone: 9876543210</span>
+              <span className="text-md text-gray-500">Phone: {data.phone}</span>
             </div>
             <div className="grid w-1/2 h-fit">
               <span className="font-bold">Location</span>
-              <span className="text-md text-gray-500">
-                San Francisco, CA
-              </span> 
+              <span className="text-md text-gray-500">{data.address}</span>
             </div>
           </div>
         </div>
